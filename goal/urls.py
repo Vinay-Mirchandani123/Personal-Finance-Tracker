34ae77e2b2e4da66c52543b030d7b0dd58097806
Grimpso <priyankmishra184@gmail.com>
2024-03-14 12:25:56 +0530

    added dashboard

---
+++ goal/urls.py
@@ -0,0 +1,12 @@
+# from django.contrib import admin
+# from django.urls import path
+# from accounts import views
+
+
+# urlpatterns = [
+#     path("/progess", views.progress, name='progress'),
+#     path("salary/", views.salary, name='salary'),
+#     path("expense", views.expense, name='expense'),
+#     path("goal/", views.goal, name='goal')
+
+# ]
--- goal/views.py
+++ goal/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render,redirect,HttpResponse
+# from django.http import HttpResponseRedirect
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate,login,logout,get_user
+# from django.contrib import messages
+# from .models import Profile
 
-# Create your views here.
+# # Create your views here.
+
+# def progress(request):
+#     return render(request, 'goal/mainbase.html')
+
+# def salary(request):
+#     return render(request, 'goal/salary.html')
+
+# def expense(request):
+#     return render(request, 'goal/expense.html')
+
+# def goal(request):
+#     return render(request, 'goal/goal.html')
+
+
+
